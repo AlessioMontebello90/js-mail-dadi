812e1a3eae5a36aa5ce32c5db43a47fe1c454d86
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-07-24 16:40:53 +0200

    fix  mail ex

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,25 @@
 //INDIRIZZI EMAIL ACCETTATI
 
 const validEmails = [
-  "admin@admin.it",
-  "pinco@admin.it",
-  "pallo@email.it",
-  "panco@email.it",
+  "raffaello@admin.it",
+  "donatello@admin.it",
+  "michelangello@email.it",
+  "leonardo@email.it",
 ];
 const userMail = prompt("Qual è il tuo indirizzo email ?");
 
 // ELEMENTO HTML NEL QUALE SCRIVERE SE L'INPUT E' OK O NO
 const textEl = document.getElementById("user");
+
+// INDIVIDUAZIONE INDIRIZZI AUTORIZZATI
+
+for (let i = 0; i < validEmails.length; i++) {
+  const validEmail = validEmails[i];
+
+  if (validEmail === userMail) {
+    textEl.innerHTML = "Benvenuto il maestro Shredder ti aspetta";
+    break;
+  } else {
+    textEl.innerHTML = "NON PUIOI ENTRARE. Hai almeno una fetta di piazza?";
+  }
+}
