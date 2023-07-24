23aab67f2b4ec0231fb2bbbcbe3053eb1d33d398
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-07-24 16:29:34 +0200

    ADD JS MAIL EX

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+//INDIRIZZI EMAIL ACCETTATI
+
+const validEmails = [
+  "admin@admin.it",
+  "pinco@admin.it",
+  "pallo@email.it",
+  "panco@email.it",
+];
+const userMail = prompt("Qual è il tuo indirizzo email ?");
+
+// ELEMENTO HTML NEL QUALE SCRIVERE SE L'INPUT E' OK O NO
+const textEl = document.getElementById("myP");
